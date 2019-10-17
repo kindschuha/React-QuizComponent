a4535d5cfac7c862df2db7d2817fcccbdf3b9fae
apk7141 <tkindschuh@sfgmembers.com>
2019-10-17 10:34:32 -0500

    Fix class name on quiz div

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component{
     }
 
     render(){
-        return <div class='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div>
+        return <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
     }
 }
 
