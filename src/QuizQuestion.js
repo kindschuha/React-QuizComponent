2618987720835a15bda5bd62b2f67d219274932a
apk7141 <tkindschuh@sfgmembers.com>
2019-10-17 15:26:57 -0500

    Module 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,11 +5,19 @@ class QuizQuestion extends Component{
     
     constructor(props){
         super(props);
+
+        this.state = {
+            incorrectAnswer: false
+        }
     }
 
     handleClick(buttonText){
-        if(buttonText === this.props.quiz_question.answer){
+        let incorrectAnswer = buttonText !== this.props.quiz_question.answer
+        this.setState((state) => ({incorrectAnswer}))
+
+        if(!incorrectAnswer){
             this.props.showNextQuestionHandler();
+            
         }
     }
 
@@ -26,6 +34,7 @@ class QuizQuestion extends Component{
                     
                 </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
             </main>
         );
     }
